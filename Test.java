7511d320c7cd634eecc491e2cd96664d9037618f
faisalmoinuddin99 <faisal25march99@gmail.com>
2024-06-07 19:58:07 +0530

    add one logic to test sum

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 class Test {
-	public static void main(String[] args) {
-		System.out.println("Hello Jenkins from Java") ;
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins from Java");
+
+        int[] a = {1, 2, 4, 5, 6, 6};
+
+		int sum_greater_than_three = Arrays.stream(a).filter(x -> x > 3).sum();
+
+		System.out.println("sum_greater_than_three :" + sum_greater_than_three);
 	}
 }
